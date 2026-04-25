2e97282a169e925abc82adc3f1192cefdc5303e0
Ifeoluwa00dev <ifeoluwathedaddy@gmail.com>
2026-04-24 17:34:28 -0700

    removed the edges

--- src/components/sections/ImageAutoScroller.tsx
+++ src/components/sections/ImageAutoScroller.tsx
@@ -17,10 +17,10 @@ export default function ImageAutoScroller() {
   return (
     <div className="relative w-full overflow-hidden py-6">
       {/* Left fade edge */}
-      <div className="pointer-events-none absolute inset-y-0 left-0 z-10 w-16 bg-gradient-to-r from-white to-transparent dark:from-gray-950" />
+      <div className="pointer-events-none absolute inset-y-0 left-0 z-10 w-16 " />
 
       {/* Right fade edge */}
-      <div className="pointer-events-none absolute inset-y-0 right-0 z-10 w-16 bg-gradient-to-l from-white to-transparent dark:from-gray-950" />
+      <div className="pointer-events-none absolute inset-y-0 right-0 z-10 w-16 " />
 
       {/* Scrolling track */}
       <div
